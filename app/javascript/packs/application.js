28fa379670c902ec748289595c599a30c2bef7fb
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-02-24 23:42:48 +0100

    add javascript navbar, transparent and become white

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,12 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import { initUpdateNavbarOnScroll } from '../components/navbar';
+
+document.addEventListener('turbolinks:load', () => {
+  // Call your JS functions here
+  initUpdateNavbarOnScroll();
+});
 
 
 // ----------------------------------------------------
