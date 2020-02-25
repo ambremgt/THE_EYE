67b5c1e2ede2c7010e5fabaa15e0bf86dc69f4da
Lital Levin <lital.levin@gmail.com>
2020-02-25 18:26:50 +0100

    add tags to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_152846) do
+ActiveRecord::Schema.define(version: 2020_02_25_171421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,7 +131,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_152846) do
     t.text "biography"
     t.string "city"
     t.integer "wallet", default: 50
-    t.string "speciality"
     t.string "language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
