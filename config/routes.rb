80a405331c46d4cab66aab81a41af234e7c7c0cf
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-02-25 12:27:26 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/results', to: 'pages#results'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :shotlists, only: [:show, :create, :new] do
+    resources :shot, only: [:create, :new]
+  end
+  resources :shots, only: [] do
+    resources :collaborations, only: [:create]
+  end
+  resources :shotlists, only: [:destroy]
+  resources :shots, only: [:destroy]
+  resources :collaborations, only: [:destroy]
 end
