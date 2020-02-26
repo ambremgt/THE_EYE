ce075a8ec34fc769a0d661f4737eee0dacb0abfc
Lital Levin <lital.levin@gmail.com>
2020-02-26 13:49:14 +0100

    change to interests array

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   acts_as_taggable_on :interests
-  INTERESTS = ["Landscapes", "Seascapes", "Architecture", "Timelapse", "Macro", "StopMotion", "Architecture", "Timelapse", "Drone", "Trans-trav", "Urban", "Street", "Architecture", "City", "Mountains", "Nature", "Timelapse", "GoPro", "4KVideoCamera", "Sliders", "Drone", "Shotgun", "Documentary"]
+  INTERESTS = ["Landscapes", "Seascapes", "Architecture", "Timelapse", "Macro", "StopMotion", "Drone", "Trans-trav", "Urban", "Street", "City", "Mountains", "Nature", "GoPro", "4KVideoCamera", "Sliders", "Shotgun", "Documentary"]
 
   has_one_attached :photo
   has_many :portfolio_assets
