65fe307640b7507b738fcec8e0acde16fa739d79
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-02-26 23:44:47 +0100

    form working but have to fix a lot

--- app/javascript/components/multiform.js
+++ app/javascript/components/multiform.js
@@ -1,10 +1,58 @@
+  let currentTab = 0 ;
+  let fieldset = document.querySelectorAll('fieldset') ;
 
-const multiForm = () => {
-document.querySelector('.next').addEventListener('click', (event) => {
-    document.querySelector('.block-1').classList.add('d-none');
-    document.querySelector('.block-2').classList.remove('d-none');
+const showTab = () => {
+    fieldset[currentTab].classList.remove('d-none');
+
+    if (currentTab == 0) {
+    document.querySelector('#prev').classList.add('d-none');
+  } else {
+    document.querySelector('#prev').classList.remove('d-none');
+  };
+  if (currentTab == (fieldset.length - 1)) {
+    document.querySelector('#next').classList.add('d-none');
+    document.querySelector('#sub').classList.remove('d-none');
+  } else {
+    document.querySelector('#next').classList.remove('d-none');
+    document.querySelector('#sub').classList.add('d-none');
+  };
+  // ... and run a function that displays the correct step indicator:
+
+  fixStepIndicator(currentTab)
+}
+const fixStepIndicator = (currentTab) => {
+  // This function removes the "active" class of all steps...
+  let i = currentTab ;
+  let x = document.querySelectorAll('#progressbar > li');
+  for (i = currentTab; i < x.length; i++) {
+    x[i].classList.remove('active');
   }
+  //... and adds the "active" class to the current step:
+  x[currentTab].classList.add('active');
 }
 
-  export { multiForm };
 
+
+
+
+
+
+
+  // Otherwise, display the correct tab:
+  const multiForm = () => {
+    if (currentTab < fieldset.length) {
+document.querySelector('#next').addEventListener('click', (event) => {
+  fieldset[currentTab].classList.add('d-none');
+  currentTab = currentTab + 1;
+  showTab(currentTab);
+  });
+
+document.querySelector('#prev').addEventListener('click', (event) => {
+  fieldset[currentTab].classList.add('d-none');
+  currentTab = currentTab - 1;
+  showTab(currentTab);
+});
+}
+}
+
+export { multiForm };
