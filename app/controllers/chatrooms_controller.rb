d0f541903df4116d1f47be8178355bfdaf5c1219
Lital Levin <lital.levin@gmail.com>
2020-02-27 11:46:23 +0100

    path

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,10 +1,24 @@
 class ChatroomsController < ApplicationController
 
+  def create
+    @chatroom = Chatroom.new
 
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render "chatrooms/show"
+    end
+  end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
 
+
+
 end
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_one_attached :photo
   has_many :portfolio_assets
 
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
