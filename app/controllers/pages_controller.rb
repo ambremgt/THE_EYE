80beea2ccade34dc3f2fbeb027e7e0e32508741d
Ambre Margerit <ambremgt@gmail.com>
2020-02-28 15:51:27 +0100

    cards clikable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,13 +13,11 @@ class PagesController < ApplicationController
 
   def results
     @filmmakers = User.all
-
     if params[:query].present?
       sql_query = "city ILIKE :query OR biography ILIKE :query"
       @filmmakers = User.where(sql_query, query: "%#{params[:query]}%")
     elsif params[:tag].present?
       @filmmakers = User.tagged_with(params[:tag])
-
     end
   end
 end
