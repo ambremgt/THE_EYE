67ff61652d978a5d74abf6fff9a69d47dbb0387a
Lital Levin <lital.levin@gmail.com>
2020-03-01 14:12:40 +0100

    add send button to form

--- app/javascript/components/attachShotlist.js
+++ app/javascript/components/attachShotlist.js
@@ -4,6 +4,14 @@ const attachShotlist = () => {
     data.preventDefault()
     const msgInput = document.querySelector('#message_content');
     msgInput.value = data.target
+
+    msgInput.insertAdjacentHTML('beforebegin',
+      `<div class="attachment-input">
+        <p>${data.target.innerText}</p>
+        <input class="form-control" value="SEND" type="submit" name="message[content]" id="message_content">
+      </div>
+
+      `)
     //document.getElementById('notif-counter').insertAdjacentHTML('beforeend',
       //`<span class="msg-number">${data.notif_counter}</span>`)
   })
