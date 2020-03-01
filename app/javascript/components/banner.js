612aa134bf8f2e5ac4e0410811ddf7f3f7200db9
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-01 17:30:34 +0100

    add style to home page

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,11 +2,13 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["The first ^1000 filmmakers's community.", "Collaboration, ^500 Innovation, ^500 Creativity.", "Camea ^2000 project"],
-    shuffle: false,
-    typeSpeed: 100,
+    strings: ["Community of ^500 Filmmakers", "Collaboration, ^500 Innovation, ^500 Creativity", "Camea ^2000 project"],
+    typeSpeed: 80,
     loop: true,
     showCursor: false,
+    FadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 300,
   });
 }
 
