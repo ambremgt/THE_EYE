f61ba1ee2bddbc8e798743314880c896d1b60c2f
Ambre Margerit <ambremgt@gmail.com>
2020-03-02 22:13:39 +0100

    chmt btn tag + ajout effet video show

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight * 0.2) {
+      if (window.scrollY >= window.innerHeight * 0.45) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
