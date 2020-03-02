2aa99f3acc82b8c842fba5b9b2c89826a437fd46
Lital Levin <lital.levin@gmail.com>
2020-03-02 17:54:27 +0100

    add content to shot and shotlist form

--- app/models/shotlist.rb
+++ app/models/shotlist.rb
@@ -2,5 +2,7 @@ class Shotlist < ApplicationRecord
   belongs_to :user
   has_many :shots
 
+  TAGS = ["landscapes", "seascapes", "architecture", "Timelapse", "Macro", "StopMotion", "Drone", "Trans-trav", "Urban", "Street", "City", "Mountains", "Nature", "GoPro", "4KVideoCamera", "Sliders", "Shotgun", "Documentary"]
+
   acts_as_taggable_on :shotlist_tags
 end
