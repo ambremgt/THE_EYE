b1543e7cd6d4d6f4704fb1d83d4f4e527054966c
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-02 15:03:33 +0100

    jj

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,14 +1,14 @@
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Community of ^500 Filmmakers", "Collaboration, ^500 Innovation, ^500 Creativity", "Camea ^2000 project "],
-    typeSpeed: 80,
-    showCursor: false,
-    FadeOut: true,
-    fadeOutClass: 'typed-fade-out',
-    fadeOutDelay: 300,
-  });
-}
+// const loadDynamicBannerText = () => {
+//   new Typed('#banner-typed-text', {
+//     strings: ["Community of ^500 Filmmakers", "Collaboration, ^500 Innovation, ^500 Creativity", "Camea ^2000 project "],
+//     typeSpeed: 80,
+//     showCursor: false,
+//     FadeOut: true,
+//     fadeOutClass: 'typed-fade-out',
+//     fadeOutDelay: 300,
+//   });
+// }
 
-export { loadDynamicBannerText };
+// export { loadDynamicBannerText };
