c1448b8a0a33098929c7bb6e0fccc89491d3eb8c
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-03 12:39:39 +0100

    add css

--- app/javascript/components/dashbordline.js
+++ app/javascript/components/dashbordline.js
@@ -2,7 +2,7 @@ const dashboardLine = () => {
   const x = document.querySelectorAll('.line2');
   x.forEach((element) => {
     window.addEventListener('scroll', () => {
-      if ( element.getBoundingClientRect().top < 500 ) {
+      if ( element.getBoundingClientRect().top < 400 ) {
         element.classList.add('line2appear');
       } else{
         element.classList.remove('line2appear');
