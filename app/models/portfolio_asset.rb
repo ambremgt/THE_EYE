857c359691bcfb595054cb638d7f114ffc50b5f5
Ambre Margerit <ambremgt@gmail.com>
2020-03-03 12:35:56 +0100

    video poster ok

--- app/models/portfolio_asset.rb
+++ app/models/portfolio_asset.rb
@@ -1,5 +1,5 @@
 class PortfolioAsset < ApplicationRecord
   belongs_to :user
   has_many_attached :photos
-  has_one :poster_picture
+  has_one_attached :poster_picture
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -189,17 +189,27 @@ arthur.photo.attach(io: URI.open("https://images.pexels.com/photos/2379004/pexel
 arthur.interest_list.add(["Landscapes", "Urban", "Architecture", "Timelapse"])
 arthur.save!
 
-#Create 10 portfolioasset
 assetsMohamed = PortfolioAsset.new(user_id: mohamed.id)
 assetsMohamed.photos.attach(io: URI.open("https://images.pexels.com/photos/2608519/pexels-photo-2608519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"), filename: "mohamedcard.png", content_type: 'image/png')
 assetsMohamed.save!
 
-assetsSunyu = PortfolioAsset.new(user_id: sunyu.id, video_url: "47360546", poster_picture: "722357235")
-assetsSunyu = PortfolioAsset.new(user_id: sunyu.id, video_url: "18554749", poster_picture: "116825470")
-assetsSunyu = PortfolioAsset.new(user_id: sunyu.id, video_url: "239492916", poster_picture: "663890158")
-assetsSunyu.photos.attach(io: URI.open("https://images.pexels.com/photos/3768221/pexels-photo-3768221.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"), filename: "sunyucard.png", content_type: 'image/png')
 
+
+assetsSunyu = PortfolioAsset.new(user_id: sunyu.id)
+assetsSunyu.photos.attach(io: URI.open("https://images.pexels.com/photos/3768221/pexels-photo-3768221.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"), filename: "sunyucard.png", content_type: 'image/png')
 assetsSunyu.save!
+
+assetsSunyu1 = PortfolioAsset.new(user_id: sunyu.id, video_url: "14352658")
+assetsSunyu1.poster_picture.attach(io: URI.open("https://i.vimeocdn.com/video/84084640_450x250.webp"), filename: "video1.png", content_type: 'image/png')
+assetsSunyu1.save!
+assetsSunyu2 = PortfolioAsset.new(user_id: sunyu.id, video_url: "18554749")
+assetsSunyu2.poster_picture.attach(io: URI.open("https://i.vimeocdn.com/video/116825470_450x250.webp"), filename: "video2.png", content_type: 'image/png')
+assetsSunyu2.save!
+assetsSunyu3 = PortfolioAsset.new(user_id: sunyu.id, video_url: "239492916")
+assetsSunyu3.poster_picture.attach(io: URI.open("https://i.vimeocdn.com/video/663890158_450x250.webp"), filename: "video3.png", content_type: 'image/png')
+assetsSunyu3.save!
+
+
 
 assetsMatheus = PortfolioAsset.new(user_id: matheus.id)
 assetsMatheus.photos.attach(io: URI.open("https://images.pexels.com/photos/1540983/pexels-photo-1540983.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"), filename: "matheuscard.png", content_type: 'image/png')
