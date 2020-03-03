d96489f413775350366f6570be3099d1c1f3a2ad
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-03 19:32:12 +0100

    add things

--- app/javascript/components/dashbordline.js
+++ app/javascript/components/dashbordline.js
@@ -1,6 +1,5 @@
 const dashboardLine = () => {
   const x = document.querySelectorAll('.line2');
-   const y = document.querySelectorAll('.lineblock');
   x.forEach((element) => {
     window.addEventListener('scroll', () => {
       if ( element.getBoundingClientRect().top < 480 ) {
@@ -8,16 +7,6 @@ const dashboardLine = () => {
 
       } else{
         element.classList.remove('line2appear', 'animated', 'fadeInRight');
-      }
-    });
-  });
-  y.forEach((element) => {
-    window.addEventListener('scroll', () => {
-      if ( element.getBoundingClientRect().top < 480 ) {
-        element.classList.add('line2appear', 'animated', 'fadeInLeft');
-
-      } else{
-        element.classList.remove('line2appear', 'animated', 'fadeInLeft');
       }
     });
   });
