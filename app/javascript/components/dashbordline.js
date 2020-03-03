84be6eabb691e7144574b7eebfd02461b734240e
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-03 13:21:09 +0100

    fix line appear

--- app/javascript/components/dashbordline.js
+++ app/javascript/components/dashbordline.js
@@ -1,14 +1,27 @@
 const dashboardLine = () => {
   const x = document.querySelectorAll('.line2');
+   const y = document.querySelectorAll('.lineblock');
   x.forEach((element) => {
     window.addEventListener('scroll', () => {
-      if ( element.getBoundingClientRect().top < 400 ) {
-        element.classList.add('line2appear');
+      if ( element.getBoundingClientRect().top < 480 ) {
+        element.classList.add('line2appear', 'animated', 'fadeInRight');
+
       } else{
-        element.classList.remove('line2appear');
+        element.classList.remove('line2appear', 'animated', 'fadeInRight');
       }
     });
   });
+  y.forEach((element) => {
+    window.addEventListener('scroll', () => {
+      if ( element.getBoundingClientRect().top < 480 ) {
+        element.classList.add('line2appear', 'animated', 'fadeInLeft');
+
+      } else{
+        element.classList.remove('line2appear', 'animated', 'fadeInLeft');
+      }
+    });
+  });
+
 }
 
 export { dashboardLine };
