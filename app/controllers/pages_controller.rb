e25f2afe5164319e6515c5257197f5048746bc6b
Lital Levin <lital.levin@gmail.com>
2020-03-03 15:32:30 +0100

    filter by location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,14 +23,14 @@ class PagesController < ApplicationController
 
       @filmmakers = User.tagged_with(params[:tag])
 
-    #elsif params[:shotlist_tag_list].present?
+    elsif params[:shotlist_tag_list].present?
       # show all filmmakers whose user tags include at least one shotlist tag
-    #  @filmmakers = User.tagged_with(params[:shotlist_tag_list].downcase.split)
+      @filmmakers = User.tagged_with(params[:shotlist_tag_list].downcase.split, :any => true)
 
-    #elsif params[:location].present?
+    elsif params[:location].present?
 
-    #  sql_query = "city ILIKE :location"
-    #  @filmmakers = User.where(sql_query, location: "%#{params[:location]}%")
+      sql_query = "city ILIKE :location"
+      @filmmakers = User.where(sql_query, location: "%#{params[:location]}%")
 
     elsif params[:location].present? && params[:shotlist_tag_list].present?
       @filmmakers = User.tagged_with(params[:shotlist_tag_list].downcase.split).filter_by_city(params[:location])
