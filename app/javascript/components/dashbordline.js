6f8234b2d333b9b784e56e315372439ce5c5a956
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-04 11:46:31 +0100

    change linear home page

--- app/javascript/components/dashbordline.js
+++ app/javascript/components/dashbordline.js
@@ -2,7 +2,7 @@ const dashboardLine = () => {
   const x = document.querySelectorAll('.line2');
   x.forEach((element) => {
     window.addEventListener('scroll', () => {
-      if ( element.getBoundingClientRect().top < 480 ) {
+      if ( element.getBoundingClientRect().top < 600 ) {
         element.classList.add('line2appear', 'animated', 'fadeInRight');
 
       } else{
