da726202447e4452ec2eb2d30fc340c9847dc845
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-03-05 23:56:37 +0100

    add..

--- app/javascript/components/footer.js
+++ app/javascript/components/footer.js
@@ -1,14 +1,14 @@
-const initUpdateFooterOnScroll = () => {
-  const navbar = document.querySelector('.footer');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight * 0.4) {
-        navbar.classList.remove('d-none');
-      } else {
-        navbar.classList.add('d-none');
-      }
-    });
-  }
-}
+// const initUpdateFooterOnScroll = () => {
+//   const navbar = document.querySelector('.footer');
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight * 0.4) {
+//         navbar.classList.remove('d-none');
+//       } else {
+//         navbar.classList.add('d-none');
+//       }
+//     });
+//   }
+// }
 
-export { initUpdateFooterOnScroll };
+// export { initUpdateFooterOnScroll };
--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,14 +1,14 @@
-const initUpdateNavbarOnScroll = () => {
-  const navbar = document.querySelector('.navbar');
-  if (navbar) {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight * 0.45) {
-        navbar.classList.add('navbar-lewagon-white');
-      } else {
-        navbar.classList.remove('navbar-lewagon-white');
-      }
-    });
-  }
-}
+// const initUpdateNavbarOnScroll = () => {
+//   const navbar = document.querySelector('.navbar');
+//   if (navbar) {
+//     window.addEventListener('scroll', () => {
+//       if (window.scrollY >= window.innerHeight * 0.45) {
+//         navbar.classList.add('navbar-lewagon-white');
+//       } else {
+//         navbar.classList.remove('navbar-lewagon-white');
+//       }
+//     });
+//   }
+// }
 
-export { initUpdateNavbarOnScroll };
+// export { initUpdateNavbarOnScroll };
