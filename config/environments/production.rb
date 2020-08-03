9f9a831daf4d4b48fec760b231b541ebc8a16fc3
Dorian Roche Philis <dodolkako@yahoo.fr>
2020-08-03 02:47:23 +0200

    ll

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,8 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-   config.action_cable.url = 'wss://cameaproject.herokuapp/cable'
-   config.action_cable.allowed_request_origins = [ "https://cameaproject.herokuapp", "https://cameaproject.herokuapp" ]
+   config.action_cable.url = 'wss://cameaproject.herokuapp,com/cable'
+   config.action_cable.allowed_request_origins = [ "https://cameaproject.herokuapp.com", "http://cameaproject.herokuapp.com" ]
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
